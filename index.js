1c9f550cebce0fa3ebb7e80ea789e2301b23cd87
Arife Kara <arifedagkara@gmail.com>
2022-10-15 11:12:37 +0300

    odev13 tamamlandi

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
-let saveEl = document.getElementById("save-el")
-let gecmisBilgi = localStorage.getItem("gecmisBilgi")
-if( gecmisBilgi !== null) {
-    saveEl.textContent = localStorage.getItem("gecmisBilgi")
+let saveEl = document.getElementById("save-el");
+let gecmisBilgi = localStorage.getItem("gecmisBilgi") ?? 0;
+let toplamYolcu = document.querySelector("#toplam-el");
+let yolcuBilgisi = localStorage.getItem("yolcuBilgisi") ?? 0;
+let total = parseInt(localStorage.getItem("totalYolcu") ?? 0);
+
+if (gecmisBilgi !== null) {
+  saveEl.textContent = localStorage.getItem("gecmisBilgi");
 } else {
-    saveEl.textContent = ""
+  saveEl.textContent = "";
 }
 
 
-let countEl = document.getElementById("count-el")
-let count = 0
+toplamYolcu.textContent =  `Toplam yolcu: ${total}`;
+
+let countEl = document.getElementById("count-el");
+let count = 0;
+
 
 function increment() {
-    count += 1
-    countEl.textContent = count
+  count += 1;
+  countEl.textContent = count;
 }
+
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    localStorage.setItem( "gecmisBilgi", saveEl.textContent )
-    countEl.textContent = 0
-    count = 0
-}
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
 
+  localStorage.setItem("gecmisBilgi", saveEl.textContent);
+
+  total += count;
+  countEl.textContent = 0;
+  count = 0;
+
+  toplamYolcu.textContent = `Toplam yolcu: ${total}`;
+  localStorage.setItem("totalYolcu", total);
+}
